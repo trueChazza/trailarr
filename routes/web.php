7ba21e8889f154d5145ce1d0da802d2bde6b9eb9
Chaz Goodare <cgnetwork.nz@gmail.com>
2021-10-11 10:24:54 +1300

    feat(router): add home, add item

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $items = Http::get('https://api.themoviedb.org/3/movie/upcoming?api_key=' . config('services.tmdb.api_key') . '&language=en-US&page=1');
+
+    return view('home', [
+        'items' => $items->json()
+    ]);
+});
+
+Route::get('/{id}', function ($id) {
+
+    $item = Http::get("https://api.themoviedb.org/3/movie/{$id}?api_key=" . config('services.tmdb.api_key') . '&language=en-US');
+
+    return view('item', [
+        'item' => $item->json()
+    ]);
 });
